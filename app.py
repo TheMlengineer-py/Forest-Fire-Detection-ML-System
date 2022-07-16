be28341991beabe295cd63b3bfdb3d4d175aada4
Dydatatech <dayooyeyemi@gmail.com>
2022-07-16 13:17:51 +0100

    init

---
+++ app.py
@@ -0,0 +1,50 @@
+import pickle
+from flask import Flask, render_template, request, app, jsonify, url_for
+import numpy as np
+import pandas as pd 
+
+
+application  = Flask(__name__)
+model   = pickle.load(open('model.pkl', 'rb'))
+
+@application.route('/')
+def home():
+    return render_template('index.html')
+
+
+@application.route('/predict_api',methods=['POST'])
+def predict_api():
+
+    data=request.json['data']
+    print(data)
+    new_data=[list(data.values())]
+    output=round(model.predict(new_data)[0])
+    if output == 1:
+        # val_ = 'Yes'
+        output = 'fire'
+    else:
+        # val_ = 'no'
+        output = 'no fire'
+    return jsonify(output)
+
+@application.route('/predict',methods=['POST'])
+def predict():
+
+    data=[float(x) for x in request.form.values()]
+    final_features = [np.array(data)]
+    print(data)
+    
+    output=model.predict(final_features)[0]
+    # print(output[0])
+    if round(output) == 1:
+        val_ = 'Yes'
+        output = 'fire'
+    else:
+        val_ = 'no'
+        output = 'no fire'
+
+    return render_template('index.html', prediction_text="Model Predict : {0}, there is {1}.".format(val_, output))
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
